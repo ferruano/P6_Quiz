236ee723bc186024261952c79f441803d5b3fae7
ferruano <36645412+ferruano@users.noreply.github.com>
2018-06-09 19:14:13 +0200

    practica8

--- controllers/tip.js
+++ controllers/tip.js
@@ -75,3 +75,47 @@ exports.destroy = (req, res, next) => {
     .catch(error => next(error));
 };
 
+// GET /quizzes/:quizId/edit////////////////
+exports.edit = (req, res, next) => {
+
+    const {quiz, tip} = req;
+
+    res.render('tips/edit', {quiz, tip});
+};
+
+exports.update = (req, res, next) => {
+
+    const {quiz, body, tip} = req;
+
+    tip.text = body.text;
+    tip.accepted = false;
+    
+
+    tip.save({fields: ["text", "accepted"]})
+    .then(quiz => {
+        req.flash('success', 'Tip edited successfully.');
+        res.redirect('/quizzes/' + quiz.id);
+    })
+    .catch(Sequelize.ValidationError, error => {
+        req.flash('error', 'There are errors in the form:');
+        error.errors.forEach(({message}) => req.flash('error', message));
+        res.render('tips/edit', {quiz, tip});
+    })
+    .catch(error => {
+        req.flash('error', 'Error editing the Tip: ' + error.message);
+        next(error);
+    });
+};
+
+exports.adminOrAuthorRequired = (req, res, next) => {
+
+    const isAdmin  = !!req.session.user.isAdmin;
+    const isAuthor = req.tip.authorId === req.session.user.id;
+
+    if (isAdmin || isAuthor) {
+        next();
+    } else {
+        console.log('Prohibited operation: The logged in user is not the author of the tip, nor an administrator.');
+        res.send(403);
+    }
+};
